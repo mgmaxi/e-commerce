a246243fe018ebe0baaa8764b43509527faeee25
mgmaxi <maximiliano.ghirardi@gmail.com>
2022-08-27 18:04:59 -0300

    Update shop-data and components to use firebase database and create nested routes for shop.

--- src/components/category-item/category-item.js
+++ src/components/category-item/category-item.js
@@ -4,14 +4,14 @@ import './category-item.scss';
 const CategoryItem = ({ category }) => {
 	const { title, imageUrl } = category;
 	return (
-		<div className="category-container">
+		<div className="category-item-container">
 			<div
 				className="background-image"
 				style={{
 					backgroundImage: `url(${imageUrl})`,
 				}}
 			/>
-			<div className="category-body-container">
+			<div className="body">
 				<h2>{title}</h2>
 				<p>Shop Now</p>
 			</div>
--- src/routes/shop/shop.js
+++ src/routes/shop/shop.js
@@ -1,20 +1,14 @@
-import { useContext } from 'react';
-import { ProductsContext } from '../../contexts/products.context';
-import ProductCard from '../../components/product-card/product-card';
-
+import { Routes, Route } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.js';
+import Category from '../category/category.js';
 import './shop.scss';
 
 const Shop = () => {
-	const { products } = useContext(ProductsContext);
-
 	return (
-		<div>
-			<div className="products-container">
-				{products.map(product => (
-					<ProductCard key={product.id} product={product} />
-				))}
-			</div>
-		</div>
+		<Routes>
+			<Route index element={<CategoriesPreview />} />
+			<Route path=":category" element={<Category />} />
+		</Routes>
 	);
 };
 
